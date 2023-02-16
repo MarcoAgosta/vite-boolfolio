3e2a78f01a711572ca838a5e8cd6c284cf1be126
MarcoAgosta <agostamarco@libero.it>
2023-02-16 18:47:47 +0100

    Creati un index e uno show dei progetti

---
+++ src/pages/projects/Show.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from "axios";
+
+export default{
+    name:"ProjectShow",
+    data(){
+        return{
+            backedURL: "http://127.0.0.1:8000/",
+
+            project:{}
+        }
+    },
+    methods:{
+        fetchProject(){
+            axios.get(this.backedURL+"api/projects/"+this.$route.params.id).then((resp)=>{
+            this.project=resp.data
+            })
+        }
+    },
+    mounted(){
+        this.fetchProject();
+    }
+}
+</script>
+
+<template>
+
+    <div class="container">
+
+        <h3 class="text-center">{{ project.name }}</h3>
+
+        <img :src="backedURL + 'storage/' + project.cover_img" class="w-100" alt="Immagine del progetto">
+
+        <p class="mt-3">{{ project.description }}</p>
+
+        <h5 class="d-inline m-2">Link al progetto:</h5><a href="#">{{ project.github_link }}</a>
+
+    </div>
+
+</template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 
 import HomePage from "./pages/home.vue";
 import ProjectsIndexPage from "./pages/projects/Index.vue";
+import ProjectShowPage from "./pages/projects/Show.vue";
 
 
 const router=createRouter({
@@ -16,6 +17,11 @@ const router=createRouter({
             path:"/projects",
             name:"projects.index",
             component: ProjectsIndexPage
+        },
+        {
+            path:"/projects/:id",
+            name:"projects.show",
+            component: ProjectShowPage
         }
     ]
 })
