e003e44f39e7167c3fbc6ec5d923f17dc5d7a91b
MarcoAgosta <agostamarco@libero.it>
2023-02-17 15:49:00 +0100

    Creato ProjectCard

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+    props:{
+        project: Object
+    },
+    data(){
+        return{
+            backedURL: "http://127.0.0.1:8000",
+        }
+        
+    }
+}
+</script>
+
+<template>
+<div class="card w-75">
+  <img class="card-img-top" :src="backedURL + '/storage/' + project.cover_img" alt="Immagine card progetto">
+
+
+  <div class="card-body">
+    <h5 class="card-title">{{ project.name }}</h5>
+    <p class="card-text">{{ project.description }}</p>
+    <router-link class="btn btn-primary" :to="'/projects/' + project.id">Seleziona progetto</router-link>
+  </div>
+</div>
+</template>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,31 @@
 <script>
+import ProjectCard from "../components/ProjectCard.vue"
+import axios from "axios"
+
 export default{
-    name:"Home"
+    components: {ProjectCard},
+    name:"Home",
+    data(){
+        return{
+            backedURL: "http://127.0.0.1:8000",
+
+            projects:[]
+        }},
+    mounted(){
+        axios.get(this.backedURL + "/api/projects").then((resp) => {
+            this.projects = resp.data;
+            })
+    }
 }
 </script>
 
 <template>
     <div class="container">
-        <h2>Questa è la pagina Home</h2>
+        <h2 class="text-center">Questa è la pagina Home</h2>
+        <div class="row mt-5">
+            <div class="col-4" v-for="project in projects">
+            <ProjectCard :project="project"></ProjectCard>
+            </div>
+        </div>
     </div>
 </template>
